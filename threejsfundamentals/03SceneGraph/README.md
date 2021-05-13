725a0fc0cb878b4808d38acb50f4fa17b5ff2f6a
rookielzy <slimjetlzy@gmail.com>
2021-05-13 16:07:39 +0800

    feat: scene part 1

---
+++ threejsfundamentals/03SceneGraph/README.md
@@ -0,0 +1,13 @@
+# 场景图
+
+`Three.js` 最重要的核心就是它的场景图（Scene Graph)。场景图就如同一个树的层级结构，每个节点都代表了一个局部空间。如下图所示：
+
+![scenegraph-generic](../images/scenegraph-generic.svg)
+
+我们举个太阳系的例子来加深理解：
+
+![scenegraph-solarsystem](../images/scenegraph-solarsystem.svg)
+
+太阳与地球
+
+![scenegraph-sun-earth](../images/scenegraph-sun-earth.svg)
--- threejsfundamentals/demo/src/script.js
+++ threejsfundamentals/demo/src/script.js
@@ -10,117 +10,55 @@ const renderer = new THREE.WebGLRenderer({canvas})
 renderer.setSize(canvas.clientWidth, canvas.clientHeight, false)
 
 // 2. 摄像机
-const fov = 75
+const fov = 40
 const aspect = canvas.clientWidth / canvas.clientHeight
 const near = 0.1
-const far = 5
+const far = 1000
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-camera.position.z = 2
+// 默认从高处垂直于原点往下看
+camera.position.set(0, 150, 0)
+camera.up.set(0, 0, 1)
+camera.lookAt(0, 0, 0)
 
 // 3. 场景
 const scene = new THREE.Scene()
 
-// 4. 光源
+const objects = []
+
+// 4. 星体对象
+const radius = 1
+const widthSegments = 6
+const heightSegments = 6
+const sphereGeometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments)
+
+// sun  默认位置在原点
+const sunMaterial = new THREE.MeshPhongMaterial({emissive: 0xffff00})
+const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial)
+sunMesh.scale.set(5, 5, 5)  // 放大太阳的尺寸大小
+scene.add(sunMesh)
+objects.push(sunMesh)
+
+// 光源默认位置在原点，在此加上光源，模拟太阳发出的光
 {
   const color = 0xffffff
-  const intensity = 1
-  const light = new THREE.AmbientLight(color, intensity)
+  const intensity = 3
+  const light = new THREE.PointLight(color, intensity)
   scene.add(light)
 }
 
-const objects = []
-
-// BoxGeometry
-{
-  const width = 0.1
-  const height = 0.1
-  const depth = 0.1
-  const widthSegments = 4
-  const heightSegments = 4
-  const depthSegments = 4
-  const geometry = new THREE.BoxGeometry(width, height, depth, widthSegments, heightSegments, depthSegments)
-  const material = new THREE.MeshPhongMaterial({color: 0x44aa88, wireframe: true})
-  const box = new THREE.Mesh(geometry, material)
-
-  box.position.x = -1
-  box.position.y = 1
-
-  objects.push(box)
-  scene.add(box)
-}
-
-{
-  const radius = 0.1
-  const segments = 24
-  const thetaStart = Math.PI * 0.25 // 起始角度
-  const thetaLength = Math.PI * 1.5 // 圆形扇区的中心角
-  const geometry = new THREE.CircleGeometry(radius, segments, thetaStart, thetaLength)
-  const material = new THREE.MeshPhongMaterial({color: 0xaa4488, wireframe: true})
-  const circle = new THREE.Mesh(geometry, material)
-
-  circle.position.x = -0.5
-  circle.position.y = 1
-
-  objects.push(circle)
-  scene.add(circle)
-}
-
-{
-  const radius = 0.1
-  const height = 0.2
-  const radialSegments = 16
-  const heightSegments = 2
-  const openEnded = true
-  const thetaStart = Math.PI * 0.25
-  const thetaLength = Math.PI * 1.5
-  const geometry = new THREE.ConeGeometry(radius, height, radialSegments, heightSegments, openEnded, thetaStart, thetaLength)
-  const material = new THREE.MeshPhongMaterial({color: 0xaa8844, wireframe: true})
-  const cone = new THREE.Mesh(geometry, material)
-
-  cone.position.x = 0
-  cone.position.y = 1
-
-  objects.push(cone)
-  scene.add(cone)
-}
-
-{
-  class CustomSinCurve extends THREE.Curve {
-    constructor(scale) {
-      super()
-      this.scale = scale
-    }
-
-    getPoint(t) {
-      const tx = t * 3 - 1.5
-      const ty = Math.sin(2 * Math.PI * t)
-      const tz = 0
-
-      return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale)
-    }
-  }
-
-  const path = new CustomSinCurve(0.3)
-  const tubularSegments = 20
-  const radius = 0.1
-  const radialSegments = 8
-  const closed = false
-  const geometry = new THREE.TubeGeometry(path, tubularSegments, radius, radialSegments, closed)
-  const material = new THREE.MeshPhongMaterial({color: 0x8844aa, wireframe: true})
-  const tube = new THREE.Mesh(geometry, material)
-
-  tube.position.x = -1
-  tube.position.y = 0
-
-  objects.push(tube)
-  scene.add(tube)
-}
+// earth
+const earthMaterial = new THREE.MeshPhongMaterial({color: 0x2233ff, emissive: 0x112244})
+const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial)
+earthMesh.position.x = 10
+// scene.add(earthMesh) 如果直接把地球加入到场景中，地球就不会围着太阳公转了
+// 因此需要将地球成为太阳的子节点
+sunMesh.add(earthMesh)  // 此时放大了太阳间接也放大了太阳的子节点
+objects.push(earthMesh)
 
 function render(time) {
   time *= 0.001
 
   objects.forEach(obj => {
-    obj.rotation.x = time
     obj.rotation.y = time
   })
 
