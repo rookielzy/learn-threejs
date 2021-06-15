1f0b043d01222100a7a813ee6ac23f5d59289a99
rookielzy <slimjetlzy@gmail.com>
2021-06-15 23:11:28 +0800

    feat: learn threejs - amibent ligght

--- learn-threejs/src/script.js
+++ learn-threejs/src/script.js
@@ -4,7 +4,7 @@ import * as THREE from 'three'
 import Stats from 'three/examples/jsm/libs/stats.module.js'
 import dat from 'three/examples/jsm/libs/dat.gui.module.js'
 import { SceneUtils } from 'three/examples/jsm/utils/SceneUtils.js'
-import { initTrackballControls } from './utils'
+import { initCamera, addHouseAndTree } from './utils'
 
 // fps
 const stats = new Stats()
@@ -18,8 +18,7 @@ const scene = new THREE.Scene()
 // scene.overrideMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff })
 
 // Create a canera, which defines where we're looking at.
-let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.set(120, 60, 180)
+let camera = initCamera()
 
 // Create a render and set the size
 const renderer = new THREE.WebGLRenderer()
@@ -27,101 +26,46 @@ renderer.setClearColor(new THREE.Color(0x000000))
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.shadowMap.enabled = true
 
-// show axes in the screen
-const axes = new THREE.AxesHelper(20)
-// scene.add(axes)
-
-// create the ground plane
-const planeGeometry = new THREE.PlaneGeometry(180, 180)
-const planeMaterial = new THREE.MeshLambertMaterial({
-  color: 0xffffff
-})
-const plane = new THREE.Mesh(planeGeometry, planeMaterial)
-
-// rotate and position the plane
-plane.rotation.x = -0.5 * Math.PI
-plane.position.set(0, 0, 0)
-
-// add the plane to the scene
-scene.add(plane)
-
-const cubeGeometry = new THREE.BoxGeometry(4, 4, 4)
-
-for (let j = 0; j < planeGeometry.parameters.height / 5; j++) {
-  for (let i = 0; i < planeGeometry.parameters.width / 5; i++) {
-    const rnd = Math.random() * 0.75 + 0.25
-    const cubeMaterial = new THREE.MeshLambertMaterial()
-    cubeMaterial.color = new THREE.Color(rnd, 0, 0)
-    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
-
-    cube.position.z = -(planeGeometry.parameters.height / 2) + 2 + (j * 5)
-    cube.position.x = -(planeGeometry.parameters.width / 2) + 2 + (i * 5)
-    cube.position.y = 2
-
-    scene.add(cube)
-  }
-}
-
-let lookAtGeom = new THREE.SphereGeometry(2)
-let lookAtMesh = new THREE.Mesh(lookAtGeom, new THREE.MeshLambertMaterial({color: 0x00ff00}))
-scene.add(lookAtMesh)
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.7)
-directionalLight.position.set(-20, 40, 60)
-scene.add(directionalLight)
-
-const ambientLight = new THREE.AmbientLight(0x292929)
+const ambientLight = new THREE.AmbientLight('#606008', 1)
 scene.add(ambientLight)
 
+const spotLight = new THREE.SpotLight(0xffffff, 1, 100, Math.PI / 4)
+spotLight.shadow.mapSize.set(2048, 2048)
+spotLight.castShadow = true
+scene.add(spotLight)
+
+const controls = setUpControls()
+
+function setUpControls() {
+  const controls = {
+    intensity: ambientLight.intensity,
+    ambientColor: ambientLight.color.getStyle(),
+    disableSpotLight: false
+  }
+
+  const gui = new dat.GUI()
+  gui.add(controls, 'intensity', 0, 3, 0.1).onChange(() => {
+    ambientLight.color = new THREE.Color(controls.ambientColor)
+    ambientLight.intensity = controls.intensity
+  })
+  gui.addColor(controls, 'ambientColor').onChange(() => {
+    ambientLight.color = new THREE.Color(controls.ambientColor)
+    ambientLight.intensity = controls.intensity
+  })
+  gui.add(controls, 'disableSpotLight').onChange(value => {
+    spotLight.visible = !value
+  })
+
+  return controls
+}
+
+// add a simple scene
+addHouseAndTree(scene)
 
 document.getElementById('webgl-output').appendChild(renderer.domElement)
 
-camera.lookAt(scene.position)
-let trackballControls = initTrackballControls(camera, renderer)
-
-const controls = {
-  perspective: 'Perspective',
-  switchCamera () {
-    if (camera instanceof THREE.PerspectiveCamera) {
-      camera = new THREE.OrthographicCamera(window.innerWidth / -16, window.innerWidth / 16, window.innerHeight / 16, window.innerHeight / -16, -200, 500)
-      camera.position.x = 120
-      camera.position.y = 60
-      camera.position.z = 180
-      camera.lookAt(scene.position)
-      trackballControls = initTrackballControls(camera, renderer)
-      this.perspective = 'Orthographic'
-    } else {
-      camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
-      camera.position.x = 120
-      camera.position.y = 60
-      camera.position.z = 180
-      camera.lookAt(scene.position)
-      trackballControls = initTrackballControls(camera, renderer)
-      this.perspective = 'Perspective'
-    }
-  }
-}
-
-const gui = new dat.GUI()
-gui.add(controls, 'switchCamera')
-gui.add(controls, 'perspective').listen()
-
-const clock = new THREE.Clock()
-
-let step = 0
-
 function renderScene() {
-  // update the stats and the controls
-  trackballControls.update(clock.getDelta())
   stats.update()
-
-  step += 0.02
-
-  if (camera instanceof THREE.Camera) {
-    const x = 10 + (100 * Math.sin(step))
-    camera.lookAt(new THREE.Vector3(x, 10, 0))
-    lookAtMesh.position.copy(new THREE.Vector3(x, 10, 0))
-  }
 
   requestAnimationFrame(renderScene)
   
--- learn-threejs/src/utils.js
+++ learn-threejs/src/utils.js
@@ -20,3 +20,102 @@ import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls
 
   return trackballControls;
 }
+
+export function initCamera(initialPosition) {
+  const position = (initialPosition !== undefined) ? initialPosition : new THREE.Vector3(-30, 40, 30)
+
+  const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+  camera.position.copy(position)
+  camera.lookAt(new THREE.Vector3(0, 0, 0))
+
+  return camera
+}
+
+export function addHouseAndTree(scene) {
+
+  createBoundingWall(scene)
+  createGroundPlane(scene)
+  createHouse(scene)
+  createTree(scene)
+  
+  function createBoundingWall(scene) {
+    const wallLeft = new THREE.CubeGeometry(70, 2, 2)
+    const wallRight = new THREE.CubeGeometry(70, 2, 2)
+    const wallTop = new THREE.CubeGeometry(2, 2, 50)
+    const wallBottom = new THREE.CubeGeometry(2, 2, 50)
+
+    const wallMaterial = new THREE.MeshPhongMaterial({
+      color: 0xa0522d
+    })
+
+    const wallLeftMesh = new THREE.Mesh(wallLeft, wallMaterial)
+    const wallRightMesh = new THREE.Mesh(wallRight, wallMaterial)
+    const wallTopMesh = new THREE.Mesh(wallTop, wallMaterial)
+    const wallBottomMesh = new THREE.Mesh(wallBottom, wallMaterial)
+
+    wallLeftMesh.position.set(15, 1, -25)
+    wallRightMesh.position.set(15, 1, 25)
+    wallTopMesh.position.set(-19, 1, 0)
+    wallBottomMesh.position.set(49, 1, 0)
+
+    scene.add(wallLeftMesh, wallRightMesh, wallTopMesh, wallBottomMesh)
+  }
+
+  function createGroundPlane(scene) {
+    // create the ground plane
+    const planeGeometry = new THREE.PlaneGeometry(70, 50)
+    const planeMaterial = new THREE.MeshPhongMaterial({
+      color: 0x9acd32
+    })
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+    plane.receiveShadow = true
+
+    // rotate and position the plane
+    plane.rotation.x = -0.5 * Math.PI
+    plane.position.x = 15
+    plane.position.y = 0
+    plane.position.z = 0
+
+    scene.add(plane)
+  }
+
+  function createHouse(scene) {
+    const roof = new THREE.ConeGeometry(5, 4)
+    const base = new THREE.CylinderGeometry(5, 5, 6)
+
+    // create the mesh
+    const roofMesh = new THREE.Mesh(roof, new THREE.MeshPhongMaterial({color: 0x8b7213}))
+    const baseMesh = new THREE.Mesh(base, new THREE.MeshPhongMaterial({color: 0xffe4c4}))
+
+    roofMesh.position.set(25, 8, 0)
+    baseMesh.position.set(25, 3, 0)
+
+    roofMesh.receiveShadow = true
+    baseMesh.receiveShadow = true
+    roofMesh.castShadow = true
+    baseMesh.castShadow = true
+
+    scene.add(roofMesh, baseMesh)
+  }
+
+  function createTree(scene) {
+    const trunk = new  THREE.CubeGeometry(1, 8, 1)
+    const leaves = new THREE.SphereGeometry(4)
+
+    // create the mesh
+    const trunkMesh = new THREE.Mesh(trunk, new THREE.MeshPhongMaterial({color: 0x8b4513}))
+    const leavesMesh = new THREE.Mesh(leaves, new THREE.MeshPhongMaterial({color: 0x00ff00}))
+
+    // position the trunk. set y to half of height of trunk
+    trunkMesh.position.set(-10, 4, 0)
+    leavesMesh.position.set(-10, 12, 0)
+
+    trunkMesh.castShadow = true
+    trunkMesh.receiveShadow = true
+    leavesMesh.castShadow = true
+    leavesMesh.receiveShadow = true
+
+    scene.add(trunkMesh)
+    scene.add(leavesMesh)
+  }
+}
